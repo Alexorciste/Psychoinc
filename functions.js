39c2a88bae5691b83f4fe07b22cc040c6703cc99
Alexorciste <boin.alexandre@wanadoo.fr>
2020-09-29 06:12:31 +0200

    ouverture volets lateraux

---
+++ functions.js
@@ -0,0 +1,35 @@
+let sideNav = document.getElementById("sidenavHebergement");
+
+let sideNav2 = document.getElementById("sidenavRestauration");
+
+
+
+
+  function openNav() {
+
+    sideNav.style.width = "50%";
+    sideNav.style.transition = "0.8s";
+
+  };
+
+  function closeNav(){
+
+    sideNav.style.width = "0%";
+    sideNav.style.transition = "0.8s";
+
+  };
+
+
+  function openNav2() {
+
+    sideNav2.style.width = "50%";
+    sideNav2.style.transition = "0.8s";
+
+  };
+
+  function closeNav2(){
+
+    sideNav2.style.width = "0%";
+    sideNav2.style.transition = "0.8s";
+
+  };
